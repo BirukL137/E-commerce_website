d2bf954c35ec75df192dda5a7322f01a7584f32b
BirukL137 <buraf13k@gmail.com>
2023-03-10 01:17:11 +0300

    Add registration and login forms using FlaskForm and wtforms modules

--- shop/admin/forms.py
+++ shop/admin/forms.py
@@ -1,8 +1,9 @@
+# Import necessary modules
 from wtforms import BooleanField, StringField, PasswordField, validators , ValidationError
 from flask_wtf import FlaskForm, Form
 from .models import User
 
-
+# Define RegistrationForm class that inherits from FlaskForm
 class RegistrationForm(FlaskForm):
     name = StringField('Name', [validators.Length(min=4, max=25)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -13,17 +14,23 @@ class RegistrationForm(FlaskForm):
     ])
     confirm = PasswordField('Repeat Password')
 
+    # Define a custom validation function for the username field
     def validate_username(self, field):
+        # Check if the entered username already exists in the User model
         if User.query.filter_by(username=field.data).first():
+            # If the username exists, raise a validation error
             raise ValidationError('Username already in use.')
 
-        
+    # Define a custom validation function for the email field
     def validate_email(self, field):
+        # Check if the entered email already exists in the User model
         if User.query.filter_by(email=field.data).first():
+            # If the email exists, raise a validation error
             raise ValidationError('Email already registered.')
-            
+     
 
 
+# Define the LoginForm class that inherits from FlaskForm
 class LoginForm(FlaskForm):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField('New Password', [validators.DataRequired()])
