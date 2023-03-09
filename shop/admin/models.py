59ac4eae2a9ca2d04c233d74c3335f1c5f299e92
BirukL137 <buraf13k@gmail.com>
2023-03-10 01:18:02 +0300

    Add User model with SQLAlchemy for database storage

--- shop/admin/models.py
+++ shop/admin/models.py
@@ -1,18 +1,19 @@
+# Import necessary modules
 from shop import db
 from datetime import datetime
 
+# Define User model class that inherits from db.Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),unique=False, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(180),unique=False, nullable=False)
-    profile = db.Column(db.String(180), unique=False, nullable=False,default='profile.jpg')
+    password = db.Column(db.String(180), unique=False, nullable=False)
+    profile = db.Column(db.String(180), unique=False, nullable=False, default='profile.jpg')
 
+    # Define a representation function for the user model
     def __repr__(self):
         return '<User %r>' % self.username
 
-
-
-
+# Create database tables
 db.create_all()
