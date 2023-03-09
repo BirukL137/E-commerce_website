41f3c03b0b8533c5a7b4c8a448fdf6a4b94a0278
BirukL137 <buraf13k@gmail.com>
2023-03-10 01:36:33 +0300

    add routes for the products page

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from flask import render_template,session, request,redirect,url_for,flash,current_app
 from shop import app,db,photos, search
 from .models import Category,Brand,Addproduct
@@ -6,36 +7,37 @@ import secrets
 import os
 
 
+# Retrieve all brands from the database by joining with the Addproduct table
 def brands():
     brands = Brand.query.join(Addproduct, (Brand.id == Addproduct.brand_id)).all()
     return brands
 
+# Retrieve all categories from the database by joining with the Addproduct table
 def categories():
     categories = Category.query.join(Addproduct,(Category.id == Addproduct.category_id)).all()
     return categories
 
-
-
+# The main route of the application that displays a paginated list of products whose stock is greater than 0
 @app.route('/')
 def home():
     page = request.args.get('page',1, type=int)
     products = Addproduct.query.filter(Addproduct.stock > 0).order_by(Addproduct.id.desc()).paginate(page=page, per_page=8)
     return render_template('products/index.html', products=products,brands=brands(),categories=categories())
 
+# The route that handles the search functionality and displays the search results
 @app.route('/result')
 def result():
     searchword = request.args.get('q')
     products = Addproduct.query.msearch(searchword, fields=['name','desc'] , limit=6)
     return render_template('products/result.html',products=products,brands=brands(),categories=categories())
 
+# Route that displays a single product page based on the given product ID
 @app.route('/product/<int:id>')
 def single_page(id):
     product = Addproduct.query.get_or_404(id)
     return render_template('products/single_page.html',product=product,brands=brands(),categories=categories())
 
-
-
-
+# Route that displays all products belonging to a given brand
 @app.route('/brand/<int:id>')
 def get_brand(id):
     page = request.args.get('page',1, type=int)
@@ -43,7 +45,7 @@ def get_brand(id):
     brand = Addproduct.query.filter_by(brand=get_brand).paginate(page=page, per_page=8)
     return render_template('products/index.html',brand=brand,brands=brands(),categories=categories(),get_brand=get_brand)
 
-
+# Route that displays all products belonging to a given category
 @app.route('/categories/<int:id>')
 def get_category(id):
     page = request.args.get('page',1, type=int)
@@ -51,7 +53,7 @@ def get_category(id):
     get_cat_prod = Addproduct.query.filter_by(category=get_cat).paginate(page=page, per_page=8)
     return render_template('products/index.html',get_cat_prod=get_cat_prod,brands=brands(),categories=categories(),get_cat=get_cat)
 
-
+# Route that allows adding new brands to the database
 @app.route('/addbrand',methods=['GET','POST'])
 def addbrand():
     if request.method =="POST":
@@ -65,33 +67,49 @@ def addbrand():
 
 @app.route('/updatebrand/<int:id>',methods=['GET','POST'])
 def updatebrand(id):
+    # Check if the user is logged in
     if 'email' not in session:
         flash('Login first please','danger')
         return redirect(url_for('login'))
+    
+    # Get the brand to be updated from the database
     updatebrand = Brand.query.get_or_404(id)
+    
+    # Get the new brand name from the form
     brand = request.form.get('brand')
+    
+    # Update the brand in the database
     if request.method =="POST":
         updatebrand.name = brand
         flash(f'The brand {updatebrand.name} was changed to {brand}','success')
         db.session.commit()
         return redirect(url_for('brands'))
+    
+    # Render the update brand form
     brand = updatebrand.name
     return render_template('products/addbrand.html', title='Udate brand',brands='brands',updatebrand=updatebrand)
 
 
 @app.route('/deletebrand/<int:id>', methods=['GET','POST'])
 def deletebrand(id):
+    # Get the brand to be deleted from the database
     brand = Brand.query.get_or_404(id)
+    
+    # Delete the brand from the database
     if request.method=="POST":
         db.session.delete(brand)
         flash(f"The brand {brand.name} was deleted from your database","success")
         db.session.commit()
         return redirect(url_for('admin'))
+    
+    # If the request method is GET, show a warning message and redirect to admin page
     flash(f"The brand {brand.name} can't be  deleted from your database","warning")
     return redirect(url_for('admin'))
 
+
 @app.route('/addcat',methods=['GET','POST'])
 def addcat():
+    # Add a new category to the database
     if request.method =="POST":
         getcat = request.form.get('category')
         category = Category(name=getcat)
@@ -99,21 +117,32 @@ def addcat():
         flash(f'The brand {getcat} was added to your database','success')
         db.session.commit()
         return redirect(url_for('addcat'))
+    
+    # Render the add category form
     return render_template('products/addbrand.html', title='Add category')
 
 
 @app.route('/updatecat/<int:id>',methods=['GET','POST'])
 def updatecat(id):
+    # Check if the user is logged in
     if 'email' not in session:
         flash('Login first please','danger')
         return redirect(url_for('login'))
+    
+    # Get the category to be updated from the database
     updatecat = Category.query.get_or_404(id)
+    
+    # Get the new category name from the form
     category = request.form.get('category')  
+    
+    # Update the category in the database
     if request.method =="POST":
         updatecat.name = category
         flash(f'The category {updatecat.name} was changed to {category}','success')
         db.session.commit()
         return redirect(url_for('categories'))
+    
+    # Render the update category form
     category = updatecat.name
     return render_template('products/addbrand.html', title='Update cat',updatecat=updatecat)
 
@@ -121,22 +150,33 @@ def updatecat(id):
 
 @app.route('/deletecat/<int:id>', methods=['GET','POST'])
 def deletecat(id):
+    # Get the category with the given id from the Category table
     category = Category.query.get_or_404(id)
     if request.method=="POST":
+        # If the request method is POST, delete the category from the Category table
         db.session.delete(category)
+        # Flash a success message indicating the category has been deleted
         flash(f"The brand {category.name} was deleted from your database","success")
+        # Commit the changes to the database
         db.session.commit()
+        # Redirect the user to the admin page
         return redirect(url_for('admin'))
+    # If the request method is not POST, flash a warning message indicating the category cannot be deleted
     flash(f"The brand {category.name} can't be  deleted from your database","warning")
+    # Redirect the user to the admin page
     return redirect(url_for('admin'))
 
 
 @app.route('/addproduct', methods=['GET','POST'])
 def addproduct():
+    # Create a new Addproducts form object
     form = Addproducts(request.form)
+    # Get all brands from the Brand table
     brands = Brand.query.all()
+    # Get all categories from the Category table
     categories = Category.query.all()
     if request.method=="POST"and 'image_1' in request.files:
+        # If the request method is POST and an image is uploaded, get the product data from the form
         name = form.name.data
         price = form.price.data
         discount = form.discount.data
@@ -167,6 +207,7 @@ def updateproduct(id):
     brand = request.form.get('brand')
     category = request.form.get('category')
     if request.method =="POST":
+        # update the values from the form
         product.name = form.name.data 
         product.price = form.price.data
         product.discount = form.discount.data
@@ -175,6 +216,8 @@ def updateproduct(id):
         product.desc = form.discription.data
         product.category_id = category
         product.brand_id = brand
+
+        # update the product images
         if request.files.get('image_1'):
             try:
                 os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_1))
@@ -213,6 +256,7 @@ def deleteproduct(id):
     product = Addproduct.query.get_or_404(id)
     if request.method =="POST":
         try:
+            # delete the old image file from the server
             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_1))
             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_2))
             os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_3))
