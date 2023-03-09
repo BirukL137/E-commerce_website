411fc7c943ef9969b4a76faa4594406fe5b8f946
BirukL137 <buraf13k@gmail.com>
2023-03-10 01:26:08 +0300

    Add registration and login forms using FlaskForm and wtforms modules

--- shop/customers/forms.py
+++ shop/customers/forms.py
@@ -1,11 +1,10 @@
+# Import necessary modules
 from wtforms import Form, StringField, TextAreaField, PasswordField,SubmitField,validators, ValidationError
 from flask_wtf.file import FileRequired,FileAllowed, FileField
 from flask_wtf import FlaskForm
 from .model import Register
 
-
-
-
+# Create a form class for customer registration
 class CustomerRegisterForm(FlaskForm):
     name = StringField('Name: ')
     username = StringField('Username: ', [validators.DataRequired()])
@@ -21,17 +20,17 @@ class CustomerRegisterForm(FlaskForm):
     profile = FileField('Profile', validators=[FileAllowed(['jpg','png','jpeg','gif'], 'Image only please')])
     submit = SubmitField('Register')
 
+    # Validate the uniqueness of username
     def validate_username(self, username):
         if Register.query.filter_by(username=username.data).first():
             raise ValidationError("This username is already in use!")
         
+    # Validate the uniqueness of email
     def validate_email(self, email):
         if Register.query.filter_by(email=email.data).first():
             raise ValidationError("This email address is already in use!")
 
-    
-
-
+# Create a form class for customer login
 class CustomerLoginFrom(FlaskForm):
     email = StringField('Email: ', [validators.Email(), validators.DataRequired()])
     password = PasswordField('Password: ', [validators.DataRequired()])
